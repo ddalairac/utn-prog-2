24ec2e1ed2ad70f585639b34c633f4e7408b7cfb
diegodalairac <stereotipoarg@gmail.com>
2019-09-21 14:29:48 -0300

    Ej 37 Centralita

---
+++ Clases/CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        public float Duracion { get { return duracion; } }
+
+        public string NroDestino { get { return nroDestino; } }
+
+        public string NroOrigen { get { return nroOrigen; } }
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.AppendFormat("\tDuracion: {0}", Duracion);
+            str.AppendFormat("\tDestino: {0}", NroDestino);
+            str.AppendFormat("\tOrigen: {0}", NroOrigen);
+
+            return str.ToString();
+        }
+
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            int response = 0;
+
+            if (llamada2.duracion > llamada1.duracion)
+            {
+                response = -1;
+            }
+            else if (llamada2.duracion < llamada1.duracion)
+            {
+                response = 1;        
+            }
+            return response;
+        }
+
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+        }
+    }
+}
---
+++ Clases/CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Local : Llamada
+    {
+        protected float costo;
+        public float CostoLlamada { get { return CalcularCosto(); } }
+
+        public Local(Llamada llamada, float costo) : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.costo = costo;
+        }
+
+        public Local(string origen, float duracion, string destino, float costo) : base(duracion, destino, origen)
+        {
+            this.costo = costo;
+        }
+
+        private float CalcularCosto()
+        {
+            return base.Duracion * this.costo;
+        }
+        public new string Mostrar()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("\tLOCAL");
+            str.AppendLine(base.Mostrar());
+            str.AppendFormat("\tCosto: ${0:#,###.00}\n", this.CostoLlamada);
+            return str.ToString();
+        }
+
+
+    }
+}
