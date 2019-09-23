a83f99ecafe95eab48235dced3053e017be56a4b
diegodalairac <stereotipoarg@gmail.com>
2019-09-22 23:27:09 -0300

    Ejercicio 33

---
+++ Ejercicios/Ejercicio33/Libro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio33
+{
+    class Libro
+    {
+        private List<string> paginas;
+        public Libro()
+        {
+            paginas = new List<string>();
+        }
+        public string this[int index]
+        {
+            get {
+                string rta = "";
+                if (index >= 0 && index < paginas.Count)
+                {
+                    rta = paginas[index];
+                }
+                return rta;
+            }
+            set {
+                if (index >= 0 && index < paginas.Count)
+                {
+                    paginas[index] = value;
+                }
+                else if (index > 0)
+                {
+                    paginas.Add(value);
+                }
+            }
+        }
+        public string Mostrar()
+        {
+            StringBuilder str = new StringBuilder("LISTADO DE LIBROS:\n");
+            foreach(string p in this.paginas)
+            {
+                str.AppendLine(p);
+            }
+            return str.ToString();
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio33/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+El indexador leerá la página pedida, siempre y cuando el subíndice se encuentre dentro de un rango correcto, 
+sino retornará un string vacio “”. En el set, la asignará si esta ya existe.Si el índice es superior al máximo existente, 
+agregará una nueva página.
+Generar el código en el Main para probar la clase.
+*/
+namespace Ejercicio33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 33";
+            Libro libro = new Libro();
+
+            libro[0] = "Intro";
+            libro[1] = "Prologo";
+            libro[2] = "Indice";
+            libro[3] = "Capitulo 1";
+            libro[4] = "Capitulo 1.2";
+            libro[5] = "Capitulo 1.3";
+
+            Console.WriteLine(libro.Mostrar());
+
+            Console.ReadKey();
+
+        }
+    }
+}
