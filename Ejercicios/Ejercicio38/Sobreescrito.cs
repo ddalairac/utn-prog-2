7d472fe50a076b354370eabd579bcaf0fa1f92a4
Diego <Diego@d.com>
2019-09-25 19:23:35 -0300

    Ej 38

---
+++ Ejercicios/Ejercicio38/Sobreescrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio38
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            //Object.Equals(new Object(), new Sobreescrito());
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+        public override bool Equals(object o)
+        {
+            return (!(o is null) && (o is Sobreescrito))? true : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
