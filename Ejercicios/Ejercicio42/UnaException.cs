43f7bd49e26d32c43499803a7d18a8aa7005c753
yo <you@example.com>
2019-10-03 21:57:11 -0300

    excepcion

---
+++ Ejercicios/Ejercicio42/UnaException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    public class UnaException:Exception
+    {
+        public string message;
+        public UnaException(string message, Exception e ) :base(e)
+        {
+        }
+    }
+}
