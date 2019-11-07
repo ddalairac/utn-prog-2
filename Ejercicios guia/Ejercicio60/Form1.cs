aafcb29f3268929f67b0213cd50b449d6bf10c99
ddalairac <diegodalairac@gmail.com>
2019-11-06 21:23:53 -0300

    Ejercicio 60 en metodos

--- Ejercicios guia/Ejercicio60/Form1.cs
+++ Ejercicios guia/Ejercicio60/Form1.cs
@@ -13,7 +13,6 @@ namespace Ejercicio60
 {
     public partial class Form1 : Form
     {
-        String connectionStr;
         SqlConnection conexion;
         SqlCommand comando;
         string aux;
@@ -22,21 +21,34 @@ namespace Ejercicio60
         public Form1()
         {
             InitializeComponent();
-            this.connectionStr = "Data Source=LAB3PC05\\SQLEXPRESS; ";
-            this.connectionStr += "Initial Catalog =AdventureWorksLT2012; ";
-            this.connectionStr += "Integrated Security = True";
+            this.ConectarDB();
+            this.aux = this.ConsultaTabla();
+        }
 
-            this.conexion = new SqlConnection(this.connectionStr);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = this.aux;
+        }
+        private void ConectarDB()
+        {
+            String connectionStr;
+            connectionStr = "Data Source=LAB3PC05\\SQLEXPRESS; ";
+            connectionStr += "Initial Catalog =AdventureWorksLT2012; ";
+            connectionStr += "Integrated Security = True";
+
+            this.conexion = new SqlConnection(connectionStr);
 
             this.comando = new SqlCommand();
             this.comando.CommandType = System.Data.CommandType.Text;
             this.comando.Connection = this.conexion;
-
+        }
+        private string ConsultaTabla()
+        {
             comando.CommandText = "SELECT Title,FirstName,LastName,ModifiedDate FROM SalesLT.Customer  ";
             comando.CommandText += "WHERE ModifiedDate between '2005-01-12' and '2005-02-12' order by ModifiedDate desc; ";
             conexion.Open();
             SqlDataReader oDr = comando.ExecuteReader();
-            
+
             StringBuilder sb = new StringBuilder();
             while (oDr.Read())
             {
@@ -45,13 +57,9 @@ namespace Ejercicio60
                 sb.Append($"{ oDr["FirstName"].ToString()} :: ");
                 sb.AppendLine($"{ oDr["ModifiedDate"].ToString()}");
             }
-            this.aux = sb.ToString();
-
+            return  sb.ToString();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            richTextBox1.Text = this.aux;
-        }
+
     }
 }
