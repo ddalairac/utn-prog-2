66dadabfaea58f49fca156a6be1d18a5bf9c6251
diegodalairac <stereotipoarg@gmail.com>
2019-11-09 15:40:18 -0300

    Ej 63

---
+++ Ejercicios guia/Ejercicio63/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio63
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetIntervalo t = new SetIntervalo(1000);
+            t.evento += AsignarHora;
+            System.Threading.Thread hilo = new System.Threading.Thread(t.Run);
+            hilo.Start();
+        }
+
+        private void AsignarHora()
+        {
+            if (this.lblHora.InvokeRequired)
+            {
+                TempDelegado d = new TempDelegado(AsignarHora);
+                this.Invoke(d);
+            }
+            else
+            {
+                this.lblHora.Text = DateTime.Now.ToString();
+            }
+        }
+   
+    }
+}
---
+++ Ejercicios guia/Ejercicio63/setIntervalo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio63
+{
+    public delegate void TempDelegado();
+    public class SetIntervalo
+    {
+        private int intervalo;
+        private bool activo;
+        public event TempDelegado evento;
+
+        public SetIntervalo(int intervalo)
+        {
+            this.activo = false;
+            this.intervalo = intervalo;
+        }
+
+        public void Run()
+        {
+            this.activo = true;
+            do
+            {
+                System.Threading.Thread.Sleep(this.intervalo);
+                this.evento();
+            } while (this.activo);
+        }
+    }
+}
