2e407bfdf62b3f5c310672082c0e0879de6b927a
ddalairac <diegodalairac@gmail.com>
2019-11-13 22:08:49 -0300

    practica parcial falta

---
+++ Parciales/practica/20180628 - SP Lab II/20180628-SP - Alumno/Entidades/Dao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Dao<T> : IArchivos<T>
+    {
+        public bool Guardar(string rutaArchivo, T objeto)
+        {
+            try
+            {
+                /*using (*/
+                //XmlTextWriter writer = new XmlTextWriter(rutaArchivo, System.Text.Encoding.UTF8); /*) { */
+                //XmlSerializer serializer = new XmlSerializer(typeof(T));
+                //serializer.Serialize(writer, objeto);
+                ////}
+                //writer.Close();
+                return true;
+            }
+            //catch (Exception e)
+            //{
+            //    throw new ErrorArchivoException(e);
+            //}
+        }
+        public T Leer(string rutaArchivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Parciales/practica/20180628 - SP Lab II/20180628-SP - Alumno/Excepciones/NotImplementedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class NotImplementedException : Exception
+    {
+        public NotImplementedException(Exception innerException)
+               : base("Exception no implementada", innerException) { }
+    }
+}
